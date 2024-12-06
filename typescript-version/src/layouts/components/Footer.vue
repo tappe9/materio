b616651b2a21b2a8a5292e58a793db02515af1fd
tappe9 <tapppe9@gmail.com>
2024-12-07 00:48:42 +0900

    Update Footer.vue

--- typescript-version/src/layouts/components/Footer.vue
+++ typescript-version/src/layouts/components/Footer.vue
@@ -12,30 +12,11 @@
         class="mx-1"
       />
       By <a
-        href="https://themeselection.com"
+        href="https://github.com/tappe9"
         target="_blank"
         rel="noopener noreferrer"
         class="text-primary ms-1"
-      >ThemeSelection</a>
-    </span>
-    <!-- 👉 Footer: right content -->
-    <span class="d-md-flex gap-x-4 text-primary d-none">
-      <a
-        href="https://themeselection.com/license/"
-        target="noopener noreferrer"
-      >License</a>
-      <a
-        href="https://themeselection.com/"
-        target="noopener noreferrer"
-      >More Themes</a>
-      <a
-        href="https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/documentation/"
-        target="noopener noreferrer"
-      >Documentation</a>
-      <a
-        href="https://themeselection.com/support/"
-        target="noopener noreferrer"
-      >Support</a>
+      >tapppe</a>
     </span>
   </div>
 </template>
